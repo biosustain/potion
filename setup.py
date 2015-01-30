56007c317544a8ff8bbcc67182aad903166527b5
lyschoening <lars@lyschoening.de>
2015-01-30 14:39:37 +0100

    New version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.2.1',
+    version='0.3.0',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
