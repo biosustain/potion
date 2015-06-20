550a267b0e4a1e094db3d6237c11e125a6f51982
lyschoening <lars@lyschoening.de>
2015-06-20 12:30:02 +0200

    Bump version to 0.7.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.7.3',
+    version='0.7.4',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
