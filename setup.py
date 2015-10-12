04b737c00ed2d4af54019e90fef7806e1f734c25
Lars Schöning <lars@lyschoening.de>
2015-10-12 15:08:15 +0200

    Release version 0.9.0 with MongoEngine backend

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.8.1',
+    version='0.9.0',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
