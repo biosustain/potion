fa244ab3b9b6c84f9f6723ed6eb21286e3c9fe46
Lars Schöning <lars@lyschoening.de>
2015-10-22 16:01:48 +0200

    Release version 0.10.0
    Breaking internal changes to the Route() class and Resource.routes.
    Added Meta.exclude_routes and Meta.route_decorators

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.9.1',
+    version='0.10.0',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
