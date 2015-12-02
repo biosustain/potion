eb074330d417a2cf190935ae3d3008bd0efc7579
Lars Schöning <lars@lyschoening.de>
2015-12-02 13:10:10 +0100

    Fix blueprints unit tests

--- tests/test_api_blueprint.py
+++ tests/test_api_blueprint.py
@@ -1,3 +1,5 @@
+import unittest
+
 from flask import Blueprint, json
 from flask_potion import Api, fields
 from flask_potion.contrib.memory.manager import MemoryManager
@@ -5,43 +7,54 @@ from flask_potion.resource import ModelResource
 from tests import BaseTestCase
 
 
-class SampleResource(ModelResource):
-    class Schema:
-        name = fields.String()
-
-    class Meta:
-        name = "samples"
-        model = "samples"
-        manager = MemoryManager
-
-
-class SampleResource2(ModelResource):
-    class Meta:
-        name = "samples2"
-        model = "samples2"
-        manager = MemoryManager
-
-
 class BlueprintApiTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(BlueprintApiTestCase, self).setUp()
+
+        class SampleResource(ModelResource):
+            class Schema:
+                name = fields.String()
+
+            class Meta:
+                name = "samples"
+                model = "samples"
+                manager = MemoryManager
+
+
+        class SampleResource2(ModelResource):
+            class Schema:
+                name = fields.String()
+
+            class Meta:
+                name = "samples"
+                model = "samples"
+                manager = MemoryManager
+
+        self.SampleResource = SampleResource
+        self.SampleResource2 = SampleResource2
+
     def test_api_blueprint(self):
         api_bp = Blueprint("potion_blueprint", __name__.split(".")[0])
         api = Api(api_bp)
-        api.add_resource(SampleResource)
+        api.add_resource(self.SampleResource)
 
         # Register Blueprint
         self.app.register_blueprint(api_bp)
         response = self.client.get("/samples")
-        self.assert200(response)
 
     def test_api_blueprint_w_prefix(self):
         api_bp = Blueprint("potion_blueprint", __name__.split(".")[0])
         api = Api(api_bp, prefix="/api/v1")
-        api.add_resource(SampleResource)
+        api.add_resource(self.SampleResource)
 
         # Register Blueprint
         self.app.register_blueprint(api_bp)
         response = self.client.get("/api/v1/samples")
         self.assert200(response)
+
+        response = self.client.get("/api/v1/samples/schema")
+        self.assertEqual('^\\/api\\/v1\\/samples\\/[^/]+$', response.json['properties']['$uri']['pattern'])
 
     def test_multiple_blueprints(self):
         # Create Blueprints
@@ -51,20 +64,35 @@ class BlueprintApiTestCase(BaseTestCase):
         # Create Api objects, add resources, and register blueprints with app
         api1 = Api(api_bp1, prefix="/api/v1")
         api2 = Api(api_bp2, prefix="/v2")
-        api1.add_resource(SampleResource)
-        api2.add_resource(SampleResource)
-        api2.add_resource(SampleResource2)
+        api1.add_resource(self.SampleResource)
+
+        with self.assertRaises(RuntimeError):
+           api2.add_resource(self.SampleResource)
+
+        api2.add_resource(self.SampleResource2)
         self.app.register_blueprint(api_bp1)
         self.app.register_blueprint(api_bp2)
 
         # Test both endpoints
         response = self.client.get("/api/v1/samples")
         self.assert200(response)
-        response = self.client.get("/api/v2/samples2")
+
+        response = self.client.get("/api/v2/samples")
+        self.assert200(response)
+
+        response = self.client.get("/api/v1/schema")
+        self.assert200(response)
+        self.assertEqual('/api/v1/samples/schema#', response.json['properties']['samples']['$ref'])
+
         response = self.client.get("/api/v2/schema")
         self.assert200(response)
-        response = self.client.get("/api/v2/samples2")
-        self.assert200(response)
+        self.assertEqual('/api/v2/samples/schema#', response.json['properties']['samples']['$ref'])
+
+        response = self.client.get("/api/v1/samples/schema")
+        self.assertEqual('^\\/api\\/v1\\/samples\\/[^/]+$', response.json['properties']['$uri']['pattern'])
+
+        response = self.client.get("/api/v2/samples/schema")
+        self.assertEqual('^\\/api\\/v2\\/samples\\/[^/]+$', response.json['properties']['$uri']['pattern'])
 
         # Test that we have two prefix'd schemas
         response = self.client.get("/api/v1/schema")
@@ -74,7 +102,7 @@ class BlueprintApiTestCase(BaseTestCase):
         self.assert200(response)
         v2_schema = json.loads(response.data)
         assert v1_schema != v2_schema
-        assert v1_schema["properties"]["samples"] == v2_schema["properties"]["samples"]
+        assert v1_schema["properties"]["samples"] != v2_schema["properties"]["samples"]
 
         # Test that endpoints are linked to same resource
         response = self.client.post('/api/v1/samples', data={
@@ -85,8 +113,12 @@ class BlueprintApiTestCase(BaseTestCase):
             "name": "to_v2"
         })
         self.assert200(response)
-        response = self.client.get("/api/v1/samples/2")
+        response = self.client.get("/api/v1/samples/1")
+        assert response.json['$uri'] == "/api/v1/samples/1"
         self.assert200(response)
         response = self.client.get("/api/v2/samples/1")
         assert response.json['$uri'] == "/api/v2/samples/1"
         self.assert200(response)
+
+if __name__ == '__main__':
+    unittest.main()
