24808723e4983db6f8812880fd0884ad61a17821
Lars Schöning <lars@lyschoening.de>
2016-02-24 16:15:26 +0100

    Release version 0.12.2
    Adds a fields.UUID field and adds support for UUID columns in the SQLAlchemyManager

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.12.1',
+    version='0.12.2',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
