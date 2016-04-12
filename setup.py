e906f4654ee8bf3230240d4ba4e663909877c352
Lars Schöning <lars@lyschoening.de>
2016-04-12 15:23:49 +0200

    Release v0.12.4
    Fixes an issue that prevented natural_keys from being used with filters

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.12.3',
+    version='0.12.4',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
