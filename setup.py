490fb331b254179dfc75dd8c220cf2ec6376f240
Lars Schöning <lars@lyschoening.de>
2016-05-25 12:07:01 +0200

    Release v0.12.6
    Adds dependencies for proper validation of fields.DateTimeString and fields.Uri

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.12.5',
+    version='0.12.6',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
