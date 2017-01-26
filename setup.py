d106bd04a3b1f52ae29fc88b74b97ed1798d8d12
Lars Schöning <lars@lyschoening.de>
2017-01-26 15:04:24 +0100

    Release v0.13.0
    - Fixes Api.decorators not being applied to "/schema"
    - Adds POTION_DECORATE_SCHEMA_ENDPOINTS configuration option
    - Fixes TypeError when applying PATCH to some SQLAlchemy native values
    - Fixes various inconsistencies in the JSON schemas
    - Adds support for user:$id and user:$uri needs with Flask-Principal

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Potion',
-    version='0.12.6',
+    version='0.13.0',
     packages=find_packages(exclude=['*tests*']),
     url='http://potion.readthedocs.org/en/latest/',
     license='MIT',
